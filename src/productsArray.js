d44f619dfea821ab0e1b1216221649777fd3d4f1
Pande <popovski.pande@gmail.com>
2023-05-08 14:02:14 +0200

    create products array

---
+++ src/productsArray.js
@@ -0,0 +1,52 @@
+export const productsArray = [
+  {
+    name: 'Classic Pro Style Training Gloves',
+    price: 34.99,
+    url: './styles/assets/training-gloves.png'
+  },
+  {
+    name: 'Classic Boxing Gloves',
+    price: 19.59,
+    url: './styles/assets/pro-boxing-gloves.png'
+  },
+  {
+    name: 'Full Face Training Headgear',
+    price: 109.99,
+    url: './styles/assets/full-face-headgear.png'
+  },
+  {
+    name: 'Classic High Performance Headgear',
+    price: 34.99,
+    url: './styles/assets/high-performance-headgear.png'
+  },
+  {
+    name: 'Semi-Elastic Hand Wraps',
+    price: 9.99,
+    url: './styles/assets/full-face-headgear.png'
+  },
+  {
+    name: 'Traditional Hand Wraps',
+    price: 3.99,
+    url: './styles/assets/high-performance-headgear.png'
+  },
+  {
+    name: 'Semi-Elastic Hand Wraps',
+    price: 9.99,
+    url: './styles/assets/semi-elastic-wraps.png'
+  },
+  {
+    name: 'Traditional Hand Wraps',
+    price: 3.99,
+    url: './styles/assets/traditional-wraps.png'
+  },
+  {
+    name: 'Premium Leather Punching Bag',
+    price: 299.99,
+    url: './styles/assets/leather-punching-bag.png'
+  },
+  {
+    name: 'Leather Speed Punching Bag',
+    price: 54.99,
+    url: './styles/assets/leather-speed-bag.png'
+  }
+]
