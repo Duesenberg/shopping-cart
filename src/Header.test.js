a764ae83e3036b76a3a834773f62c6fba9ca9768
Pande <popovski.pande@gmail.com>
2023-05-13 13:57:18 +0200

    test header

--- src/Header.test.js
+++ src/Header.test.js
@@ -5,16 +5,25 @@ import userEvent from '@testing-library/user-event';
 import Shop from "./Shop";
 import Header from './Header';
 import { productsArray } from "./productsArray";
+import { BrowserRouter } from 'react-router-dom'
 
-describe('header feature works correctly', () => {
-  it('shows correct number of items in cart', () => {
-    render(
-      <div>
-        <Header />
-        <Shop />
-      </div>
-    );
-
-    
+describe('header renders correctly', () => {
+  it('renders logo', () => {
+    render(<BrowserRouter><Header /></BrowserRouter>);
+    expect(screen.getByRole('link',{name: 'BoxShop'})).toBeInTheDocument();
   });
+  
+  it('has a functioning logo link', () => {
+    render(<BrowserRouter><Header /></BrowserRouter>);
+    expect(screen.getByRole('link', {name: 'BoxShop'}))
+    .toHaveAttribute('href', 'https://github.com/Duesenberg?tab=repositories');
+  });
+  
+  it('shows the correct items in cart', () => {
+    let itemsInCart = 5;
+    render(<BrowserRouter><Header itemsInCart={itemsInCart} /></BrowserRouter>);
+    const tabFour = screen.getAllByRole('tab')[3];
+
+    expect(tabFour).toHaveTextContent('Cart (5)');
+  })
 });
