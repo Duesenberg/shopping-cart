8bcc666f2c22f046027587bad86ce1e30fef1d9f
Pande <popovski.pande@gmail.com>
2023-05-13 20:01:36 +0200

    test shopping cart further

--- src/ShoppingCart.test.js
+++ src/ShoppingCart.test.js
@@ -36,4 +36,53 @@ describe('basic page layout renders correctly', () => {
     expect(screen.getByTestId('empty-cart-card')).toBeInTheDocument();
     expect(screen.queryByTestId('sum-card')).not.toBeInTheDocument();
   })
-})
+});
+
+describe('prices and item amount show up correctly', () => {
+  it('shows the correct amount of items', () => {
+      const cart = [
+        {
+          name: 'Classic Pro Style Training Gloves',
+          price: 34.99,
+          url: './styles/assets/training-gloves.png',
+          amount: 3,
+          id: uniqid()
+        },
+        {
+          name: 'Classic Boxing Gloves',
+          price: 19.59,
+          url: './styles/assets/pro-boxing-gloves.png',
+          amount: 2,
+          id: uniqid()
+        }
+      ]
+      render(<ShoppingCart cart={cart} />);
+
+      expect(screen.getByText('Classic Pro Style Training Gloves x 3')).toBeInTheDocument();
+      expect(screen.getByText('Classic Boxing Gloves x 2')).toBeInTheDocument();
+  });
+
+  it('shows the correct dollar amounts', () => {
+      const cart = [
+        {
+          name: 'Classic Pro Style Training Gloves',
+          price: 34.99,
+          url: './styles/assets/training-gloves.png',
+          amount: 3,
+          id: uniqid()
+        },
+        {
+          name: 'Classic Boxing Gloves',
+          price: 19.59,
+          url: './styles/assets/pro-boxing-gloves.png',
+          amount: 2,
+          id: uniqid()
+        }
+      ]
+      render(<ShoppingCart cart={cart} />);
+
+      expect(screen.getByText('$34.99 x 3 = $104.97')).toBeInTheDocument();
+      expect(screen.getByText('$19.59 x 2 = $39.18')).toBeInTheDocument();
+      expect(screen.getByText('$144.15')).toBeInTheDocument();
+  });
+});
